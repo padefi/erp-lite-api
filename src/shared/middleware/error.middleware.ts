9ee358889a3fd702b11c463c0a1de89440891340
Pablo De Filpo <pablo.defilpo@gmail.com>
2026-04-04 00:48:13 -0300

    feat: add global error handling middleware

---
+++ src/shared/middleware/error.middleware.ts
@@ -0,0 +1,9 @@
+import type { Request, Response, NextFunction } from 'express';
+
+export const errorHandler = (err: any, _req: Request, res: Response, _next: NextFunction) => {
+    console.error(err);
+
+    res.status(500).json({
+        message: 'Internal server error',
+    });
+};
